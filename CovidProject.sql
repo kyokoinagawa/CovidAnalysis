455bad89904d1579f6e671d75b4bd478806d4016
kyokoinagawa <118694618+kyokoinagawa@users.noreply.github.com>
2023-06-29 19:50:44 -0700

    Update CovidProject.sql
    CovidVsSars

--- CovidProject.sql
+++ CovidProject.sql
@@ -134,43 +134,40 @@ from #PercentPopulationVaccinated
 
 -- Views for later visualizations --
 
+--Covid and SARS Fatality Rate Comparison
 
-Create view HighestInfectionCount as
+select cdeaths.location, MAX(cdeaths.total_deaths) as CovidDeaths, MAX(sdeaths.[Number of deaths]) as SARSDeaths
+from CovidDeaths cdeaths
+join sars_2003_complete_dataset sdeaths
+on cdeaths.location = sdeaths.location
+group by cdeaths.location
+order by SARSDeaths desc
+
+--Covid Death Percentage
+
+select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(new_cases) *100 as CovidDeathPercentage
+from CovidDeaths
+where continent is not null
+order by 1, 2
+
+
+--SARS Death Percentage 
+select SUM([Cumulative number of case(s)]) as total_cases, SUM(cast([Number of deaths]as int)) as total_deaths, SUM(cast([Number of deaths] as int))/SUM([Cumulative number of case(s)]) *100 as SarsDeathPercentage
+from sars_2003_complete_dataset
+order by 1, 2
+
+
+--SARS Global Cases
+
+select location,SUM([Cumulative number of case(s)]) as SarsCases
+from sars_2003_complete_dataset
+group by location
+order by SarsCases desc
+
+
+--Countries with Highest Infection Rate Compared to Population
+
 select location,population,MAX(total_cases) as HighestInfectionCount, (Max(total_cases)/population)*100 as PercentPopulationInfected
 from PortfolioProject..CovidDeaths
 group by location, population
-
-select * from HighestInfectionCount
-
-Create View CasesVsDeathsInJapan as
-select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage-- total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
-from PortfolioProject..CovidDeaths
-where location like '%japan%'
-
-select * from CasesVsDeathsInJapan
-
-Create View PercentPopulationVaccinated as
-select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-sum(convert(int, vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) 
-as RollingPeopleVaccinated
-from PortfolioProject..CovidDeaths dea
-join PortfolioProject..CovidVaccinations vac
-on dea.location = vac.location
-and dea.date = vac.date
-where dea.continent is not null
-
-select *
-from PercentPopulationVaccinated
-
-Create view JapanVaccinatedPopulation as
-select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-sum(convert(int, vac.new_vaccinations)) over (partition by dea.location order by dea.location, dea.date) 
-as RollingPeopleVaccinated
-from PortfolioProject..CovidDeaths dea
-join PortfolioProject..CovidVaccinations vac
-on dea.location = vac.location
-and dea.date = vac.date
-where dea.location like '%japan%'
-
-select *
-from JapanVaccinatedPopulation
+order by PercentPopulationInfected desc
